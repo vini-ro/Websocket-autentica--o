4cfd6a3e6029c9667de9a7bc3896a21e0c755018
Vinicius <vroliveira.eng@gmail.com>
2025-01-08 15:23:55 -0300

    Enviando dados de cadastro

--- servidor/socket-back.js
+++ servidor/socket-back.js
@@ -1,10 +1,13 @@
-import registrarEventosDocumento from "./registrarEventos/registrarEventosDocumento.js";
-import registrarEventosInicio from "./registrarEventos/registrarEventosInicio.js";
+import registrarEventosCadastro from "./registrarEventos/Cadastro.js";
+import registrarEventosDocumento from "./registrarEventos/Documento.js";
+import registrarEventosInicio from "./registrarEventos/Inicio.js";
+
 import io from "./servidor.js";
 
 io.on("connection", (socket) => {
   
 registrarEventosInicio(socket, io);
 registrarEventosDocumento(socket, io);
+registrarEventosCadastro(socket, io);
   
 });
