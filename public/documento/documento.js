5226dbfffac4c9d117549538e2909503ef6f5817
Vinicius <vroliveira.eng@gmail.com>
2025-01-09 12:54:04 -0300

    Atualizando Usuarios Conectados

--- public/documento/documento.js
+++ public/documento/documento.js
@@ -2,24 +2,32 @@ import {
   emitirExcluirDocumento,
   emitirTextoEditor,
   selecionarDocumento,
-} from "./socket-front-documento.js";
+} from './socket-front-documento.js';
 
 const parametros = new URLSearchParams(window.location.search);
-const nomeDocumento = parametros.get("nome");
+const nomeDocumento = parametros.get('nome');
 
-const textoEditor = document.getElementById("editor-texto");
-const tituloDocumento = document.getElementById("titulo-documento");
-const botaoExcluir = document.getElementById("excluir-documento");
+const textoEditor = document.getElementById('editor-texto');
+const tituloDocumento = document.getElementById('titulo-documento');
+const botaoExcluir = document.getElementById('excluir-documento');
+const listaUsuariosConectados = document.getElementById('usuarios-conectados');
 
-tituloDocumento.textContent = nomeDocumento || "Documento sem título";
+tituloDocumento.textContent = nomeDocumento || 'Documento sem título';
 
-function tratarAutorizacaoSucesso (payloadToken) {
-  selecionarDocumento({nomeDocumento, nomeUsuario: payloadToken.nomeUsuario});
-
+function tratarAutorizacaoSucesso(payloadToken) {
+  selecionarDocumento({ nomeDocumento, nomeUsuario: payloadToken.nomeUsuario });
 }
 
+function atualizarInterfaceUsuarios(usuariosNoDocumento) {
+  listaUsuariosConectados.innerHTML = '';
 
-textoEditor.addEventListener("keyup", () => {
+  usuariosNoDocumento.forEach((usuario) => {
+    listaUsuariosConectados.innerHTML += `          <li class="list-group-item">${usuario}</li>
+`;
+  });
+}
+
+textoEditor.addEventListener('keyup', () => {
   emitirTextoEditor({
     texto: textoEditor.value,
     nomeDocumento,
@@ -30,15 +38,20 @@ function atualizaTextoEditor(texto) {
   textoEditor.value = texto;
 }
 
-botaoExcluir.addEventListener("click", () => {
+botaoExcluir.addEventListener('click', () => {
   emitirExcluirDocumento(nomeDocumento);
 });
 
 function alertarERedirecionar(nome) {
   if (nome === nomeDocumento) {
     alert(`Documento ${nome} excluído!`);
-    window.location.href = "/";
+    window.location.href = '/';
   }
 }
 
-export { atualizaTextoEditor, alertarERedirecionar, tratarAutorizacaoSucesso };
+export {
+  atualizaTextoEditor,
+  alertarERedirecionar,
+  tratarAutorizacaoSucesso,
+  atualizarInterfaceUsuarios,
+};
